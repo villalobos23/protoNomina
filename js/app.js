aeb7a7f0807e930e9339258178d814f6fb7b6ebf
villalobos23 <villalobos23@gmail.com>
2015-12-17 22:56:53 -0430

    Ya el calculo se hace correctamente, hora de validar

--- js/app.js
+++ js/app.js
@@ -13,17 +13,22 @@ function addEmployee(){
         bsExtra = genData.querySelector("#bs-extra"),
         tabla = document.getElementById('reporte')
                         .getElementsByTagName('tbody')[0],
-        fila = tabla.insertRow(tabla.rows.length);
+        fila = tabla.insertRow(tabla.rows.length),
+        ivssAmount = (Number(ivss.value)/100)*Number(salario.value),
+        extraAmount = Number(extras.value)*Number(bsExtra.value),
+        faovAmount = Number((faov.value/100)) * Number(salario.value),
+        totalAmount = Number(salario.value)+extraAmount-ivssAmount-faovAmount-Number(prestamo.value)-Number(vale.value);
 
         fila.insertCell(0).
             appendChild(document.createTextNode(nombre.value));
         
         fila.insertCell(1).appendChild(document.createTextNode(salario.value));
-        fila.insertCell(2).appendChild(document.createTextNode(faov.value));
-        fila.insertCell(3).appendChild(document.createTextNode(ivss.value));
+        fila.insertCell(2).appendChild(document.createTextNode(faovAmount));
+        fila.insertCell(3).appendChild(document.createTextNode(ivssAmount));
         fila.insertCell(4).appendChild(document.createTextNode(prestamo.value));
-        fila.insertCell(5).appendChild(document.createTextNode(extras.value*bsExtra.value));
-        
+        fila.insertCell(5).appendChild(document.createTextNode(vale.value));
+        fila.insertCell(6).appendChild(document.createTextNode(extraAmount));
+        fila.insertCell(7).appendChild(document.createTextNode(totalAmount));
 }
 
 $(function () {
